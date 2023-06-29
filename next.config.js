164daaa364fba35f702aa032047e98ea382b704e
manishvbn <manishvbn@gmail.com>
2023-06-30 00:43:08 +0530

    Updated

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/im-query-poc' : '',
-  images: {
-    unoptimized: true,
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
